a19dda32333ae2575d6ff262806c7c033594a793
tblst8080 <123396762+tblst8080@users.noreply.github.com>
2023-01-28 15:07:52 +0800

    Update ball.py

--- ball.py
+++ ball.py
@@ -10,7 +10,7 @@ color_set = ("red", "orange", "yellow", "green", "blue", "purple")
 
 
 class Ball:
-    def __init__(self):
+    def __init__(self, preset=None):
         self.max_speed = 5
         self.flightspeed = 3
         self.speed = 2
@@ -21,13 +21,19 @@ class Ball:
 
         self.start_time = None
         self.winner = None
+
         self.body = t.Turtle(shape="circle", visible=True)
         self.body.penup()
         self.body.shapesize(self.size)
-        self.body.color("red")
+        self.body.fillcolor("red")
+        self.body.pen(outline=2, pencolor="red")
         self.body.setheading(r.randint(0, 360))
 
-        self.distance_history = None
+        try:
+            self.body.fillcolor(preset["balL_fillcolor"])
+            self.body.pen(outline=preset["ball_outline"], pencolor=preset["ball_pencolor"])
+        except:
+            pass
 
     def moving(self):
         if self.speed < 0:
@@ -41,37 +47,26 @@ class Ball:
 
     def fine_moving(self):
         self.body.forward(1)
-        self.collision_wall()
 
     def collision_wall(self):
         collision = False
-
         # upper wall
-        if HEIGHT / 2 - self.body.ycor() <= 10:
+        if HEIGHT / 2 - self.body.ycor() <= 10 and 0 < self.body.heading() < 180:
             collision = True
-            if self.body.heading() == 90:
-                self.body.right(180)
-            elif 90 < self.body.heading() < 180:
-                angle = 2 * (180 - self.body.heading())
-                self.body.left(angle)
-            elif 0 < self.body.heading() < 90:
-                angle = 2 * (self.body.heading())
-                self.body.right(angle)
+            reverse_initial = (self.body.heading() + 180) % 360
+            theta = 270 - reverse_initial
+            self.body.setheading(reverse_initial + 2 * theta)
 
         # lower wall
-        if self.body.ycor() + HEIGHT / 2 <= 10:
+        if self.body.ycor() + HEIGHT / 2 <= 10 and 180 < self.body.heading() < 360:
             collision = True
-            if self.body.heading() == 270:
-                self.body.right(180)
-            elif 270 < self.body.heading() < 360:
-                angle = 2 * (360 - self.body.heading())
-                self.body.left(angle)
-            elif 180 < self.body.heading() < 270:
-                angle = 2 * (self.body.heading() - 180)
-                self.body.right(angle)
+            reverse_initial = (self.body.heading() + 180) % 360
+            theta = 90 - reverse_initial
+            self.body.setheading(reverse_initial + 2 * theta)
 
         if collision is True and self.speed > self.min_speed:
             self.speed -= self.increment
+            collision = False
 
     def collision_paddle(self, paddles):
         activated = False  # boolean for external functions
@@ -82,7 +77,6 @@ class Ball:
                 initial_direction = self.body.heading()
                 from_pad = self.body.towards(pad["position"])  # direction from pad
                 criterion = (initial_direction - from_pad) % 360
-                print(criterion)
 
                 # Condition for if the paddle hits the ball from the front
                 if 90 >= criterion or criterion >= 270:
@@ -99,17 +93,18 @@ class Ball:
                     theta = np.radians(final_direction - force_vector)
                     final_speed = pad["speed"] * np.cos(theta)
 
-                    self.speed = final_speed*.7 + self.speed*.7
+                    self.speed = final_speed * .7 + self.speed * .7
 
                 # Condition for if the paddle hits the ball from behind ( 90 < angle < 270)
-                elif 90 < criterion < 270 or not self.movable:
+                elif 90 < criterion < 270:
                     initial_speed = self.speed
                     angle_c = np.radians(180 - initial_direction + from_pad)
-                    final_speed = np.sqrt(np.square(pad["speed"])+np.square(initial_speed)-(2*pad['speed']*initial_speed*np.cos(angle_c)))
+                    final_speed = np.sqrt(np.square(pad["speed"]) + np.square(initial_speed) - (
+                                2 * pad['speed'] * initial_speed * np.cos(angle_c)))
 
-                    self.speed = final_speed*2.5
+                    self.speed = final_speed * 2.5
 
-                    sin_theta = (initial_speed*np.sin(angle_c))/final_speed
+                    sin_theta = (initial_speed * np.sin(angle_c)) / final_speed
                     theta = np.arcsin(sin_theta)
                     d_theta = np.degrees(theta)
                     self.body.setheading(pad["direction"] + d_theta)
@@ -148,7 +143,6 @@ class Ball:
 
     def change_color(self):
         color_now = self.body.fillcolor()
-        print(color_now)
         pos = color_set.index(color_now)
         new_pos = (pos + 1) % len(color_set)
         self.body.color(color_set[new_pos])
