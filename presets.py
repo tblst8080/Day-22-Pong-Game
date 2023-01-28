5fc26d5eb86acb45fe59642e1bbb41a3fc52d09a
tblst8080 <123396762+tblst8080@users.noreply.github.com>
2023-01-28 15:09:35 +0800

    Add files via upload

---
+++ presets.py
@@ -0,0 +1,24 @@
+kanna_package = {
+    "bgpic": "kanna.gif",
+    "bgcolor": None,
+    "ball_pencolor": "red",
+    "balL_fillcolor": "purple",
+    "ball_outline": 3,
+    "paddle_pencolor": "pink",
+    "paddle_fillcolor": "",
+    "paddle_outline": 10,
+    "score_color": "purple",
+}
+
+orangeblue = {
+    "bgpic": None,
+    "bgcolor": "purple",
+    "ball_pencolor": "red",
+    "balL_fillcolor": "red",
+    "ball_outline": 3,
+    "paddle_pencolor": "orange",
+    "paddle_fillcolor": "",
+    "paddle_outline": 25,
+    "score_color": "orange",
+}
+
